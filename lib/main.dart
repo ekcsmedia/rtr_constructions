fd54333af2fec416f0477573f824029b1fdd84ba
santhosh <santosh.kumar@simplifyvms.com>
2024-05-23 00:43:05 +0530

    Firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'dart:async';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sattva_rtr_construction_split_wise/views/auth_screens/login_home_screen.dart';
 import 'package:sattva_rtr_construction_split_wise/views/home_screen/home_screen.dart';
 import 'package:sattva_rtr_construction_split_wise/views/splash_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -22,7 +25,19 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'RTR Constructions'),
+      home: FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if(snapshot.hasError) {
+            print("Error");
+          }
+          if(snapshot.connectionState == ConnectionState.done) {
+            print("connected");
+            return const MyHomePage(title: "Flutter Firebase Integration");
+          }
+          return const CircularProgressIndicator();
+        },
+      ),
     );
   }
 }
